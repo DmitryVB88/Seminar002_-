8fd379f9c80ec22a007af55d1327c07df474e396
Dima <dima.brashovyan@mail.ru>
2022-05-16 08:55:22 +0300

    Первое задание семинара

---
+++ Second_task/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите числа x и y");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Первая четверть");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
